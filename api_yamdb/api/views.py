98df6acd24f5b579e61fc8d9cc80dbcaf5b51692
Рифат Хасанов <Hidex-morfeyka@mail.ru>
2022-11-02 21:42:24 +0300

    Добавлены пермишены

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -5,6 +5,7 @@ from rest_framework import viewsets
 from rest_framework.filters import SearchFilter
 from rest_framework.permissions import AllowAny
 
+from .permissions import IsAdmin, IsAdminOrReadOnly, IsAdminModeratirAuthor
 from .serializers import (CategorySerializer, GenreSerializer, TitleSerializer)
 
 
@@ -12,21 +13,21 @@ class TitleViewSet(viewsets.ModelViewSet):
     queryset = Title.objects.all()
     serializer_class = TitleSerializer
     pagination_class = None
-    permission_classes = (AllowAny,)  #IsAdminOrReadOnly
+    permission_classes = (IsAdminOrReadOnly,)
 
 
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
     pagination_class = None
-    permission_classes = (AllowAny,)  #IsAdminOrReadOnly
+    permission_classes = (IsAdminOrReadOnly,)
 
 
 class GenreViewSet(viewsets.ModelViewSet):
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
     pagination_class = None
-    permission_classes = (AllowAny,)
+    permission_classes = (IsAdminOrReadOnly,)
     filter_backends = (SearchFilter,)
     search_fields = ('name',)
 
