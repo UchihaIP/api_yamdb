fce9a9a09e89380ce0325bbf0f01a28cca7a94aa
Роман Агеев <gegins1@mail.ru>
2022-11-02 23:17:29 +0300

    Созданы Отзывы и комменты

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,5 +1,5 @@
-from rest_framework import serializers
-from reviews.models import Category, Genre, Title
+from rest_framework import serializers, validators
+from reviews.models import Category, Genre, Title, Review, Comment
 import datetime as dt
 
 
@@ -22,7 +22,7 @@ class GenreSerializer(serializers.ModelSerializer):
 
 class TitleSerializer(serializers.ModelSerializer):
     # rating = serializers.SerializerMethodField()
-    genre = GenreSerializer()
+    genre = GenreSerializer(many=True)
     category = serializers.SlugRelatedField(
         queryset = Category.objects.all(),
         slug_field='slug',
@@ -39,4 +39,35 @@ class TitleSerializer(serializers.ModelSerializer):
         year = dt.date.today().year
         if value > year:
             raise serializers.ValidationError('Проверьте год указанного произведения.')
-        return value 
+        return value
+
+    #def create(self, validated_data):
+        #genre = validated_data.pop('genre')
+        #title = Title.objects.create(**validated_data)
+        #for i in genre:
+            #current_genre, status = Genre.objects.get_or_create(**i)
+
+
+
+class ReviewSerialiser(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(read_only=True, slug_field='username')
+
+    class Meta:
+        fields = ('id', 'text', 'author', 'score', 'pub_date')
+        model = Review
+        validators = (validators.UniqueTogetherValidator(queryset=Review.objects.all(), fields=('author', 'title')))
+        read_only_field = ('title',)
+
+    def validate(self, attrs):
+        if attrs['score'] < 1 or attrs['score'] > 10:
+            raise serializers.ValidationError('Оценка должна быть от 1 до 10!')
+        return super().validate(attrs)
+
+
+class CommentSerialiser(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(read_only=True, slug_field='username')
+
+    class Meta:
+        fields = ('id', 'text', 'author', 'pub_date')
+        model =Comment
+        read_only_field = ('review',)
