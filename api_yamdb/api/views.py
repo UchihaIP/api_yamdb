0b8cfcf03b98c0d353fd2692341e00935f3da066
Рифат Хасанов <Hidex-morfeyka@mail.ru>
2022-11-12 20:26:33 +0300

    after 3 code-review ()

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,7 +1,5 @@
 from django.contrib.auth.tokens import default_token_generator
-from django.core.exceptions import ObjectDoesNotExist
 from django.core.mail import send_mail
-from django.db import IntegrityError
 from django.db.models import Avg
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
